20787ec0cc7252101ecea23ae9d955616cd052bf
Boukri Amine <AMINE_BOUKRI@emsi-edu.ma>
2023-02-16 18:40:42 +0100

    change in payment UI

--- src/app/components/mutualAid/paiement/paiement.component.ts
+++ src/app/components/mutualAid/paiement/paiement.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaiementComponent implements OnInit {
 
+  isDonation: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
